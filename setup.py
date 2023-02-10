48ebe3218aa1822c1189aae2462019d55dfc7c6f
Kent Mewhort <kent@openissues.ca>
2023-02-10 13:18:23 -0500

    Add long desciption

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ import setuptools
 
 setuptools.setup(
     name="upscalers",
-    version="0.5.2",
+    version="0.5.3",
     description="AI upscaling of images",
+    long_description="Upscalers provides an easy way to apply image upscaling using a variety of AI upscalers (including ESRGAN, R-ESRGAN, ScuNET, and SwinIR)",
     author="Kent Mewhort",
     author_email="kent@openissues.ca",
     url='https://github.com/kmewhort/upscalers',
